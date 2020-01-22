1c5c2d4821849a7a68576a9ccbf251913dd07830
s2986104 <d.guillen@griffith.edu.au>
2020-01-23 09:21:05 +1000

    Fixed the allowed_values parameters to the Input functions. Ranges are specified as [[min, max]]

--- src/silvereye_wps_demo/processes/mean_years.py
+++ src/silvereye_wps_demo/processes/mean_years.py
@@ -27,36 +27,32 @@ class MeanYears(Process):
             LiteralInput(
                 'year_min', 'Minimum year to process, in range 1970:2014',
                 data_type='integer', min_occurs=1, max_occurs=1,
-                mode=MODE.SIMPLE, allowed_values=[i for i in range(1970, 2015)]
+                mode=MODE.SIMPLE, allowed_values=[[1970, 2014]]
             ),
             LiteralInput(
                 'year_max', 'Maximum year to process, in range 1970:2014',
                 data_type='integer', min_occurs=1, max_occurs=1,
-                mode=MODE.SIMPLE, allowed_values=[i for i in range(1970, 2015)]
+                mode=MODE.SIMPLE, allowed_values=[[1970, 2014]]
             ),
             LiteralInput(
                 'lat_min', 'Latitude minimum value to process in range -43.735:-9.005',
                 data_type='float', min_occurs=1, max_occurs=1,
-                mode=MODE.SIMPLE,
-                # allowed_values=(-43.735, -9.005)
+                mode=MODE.SIMPLE, allowed_values=[[-43.735, -9.005]]
             ),
             LiteralInput(
                 'lat_max', 'Latitude maximum value to process in range -43.735:-9.005',
                 data_type='float', min_occurs=1, max_occurs=1,
-                mode=MODE.SIMPLE,
-                # allowed_values=(-43.735, -9.005)
+                mode=MODE.SIMPLE, allowed_values=[[-43.735, -9.005]]
             ),
             LiteralInput(
                 'lon_min', 'Longitude minimum value to process in range 112.905:153.995',
                 data_type='float', min_occurs=1, max_occurs=1,
-                mode=MODE.SIMPLE,
-                # allowed_values=(112.905, 153.995)
+                mode=MODE.SIMPLE, allowed_values=[[112.905, 153.995]]
             ),
             LiteralInput(
                 'lon_max', 'Longitude maximum value to process in range 112.905:153.995',
                 data_type='float', min_occurs=1, max_occurs=1,
-                mode=MODE.SIMPLE,
-                # allowed_values=(112.905, 153.995)
+                mode=MODE.SIMPLE, allowed_values=[[112.905, 153.995]]
             ),
         ]
 
--- src/silvereye_wps_demo/processes/mean_years_one_quarter.py
+++ src/silvereye_wps_demo/processes/mean_years_one_quarter.py
@@ -25,41 +25,37 @@ class MeanYearsOneQuarter(Process):
             LiteralInput(
                 'year_min', 'Minimum year to process in range 1970:2014',
                 data_type='integer', min_occurs=1, max_occurs=1,
-                mode=MODE.SIMPLE, allowed_values=[yr for yr in range(1970, 2015)]
+                mode=MODE.SIMPLE, allowed_values=[[1970, 2014]]
             ),
             LiteralInput(
                 'year_max', 'Maximum year to process in range 1970:2014',
                 data_type='integer', min_occurs=1, max_occurs=1,
-                mode=MODE.SIMPLE, allowed_values=[yr for yr in range(1970, 2015)]
+                mode=MODE.SIMPLE, allowed_values=[[1970, 2014]]
             ),
             LiteralInput(
                 'quarter', 'Quarter to process in range 1:4',
                 data_type='integer', min_occurs=1, max_occurs=1,
-                mode=MODE.SIMPLE, allowed_values=[i for i in range(1, 5)]
+                mode=MODE.SIMPLE, allowed_values=[[1, 4]]
             ),
             LiteralInput(
                 'lat_min', 'Latitude minimum value to process in range -43.735:-9.005',
                 data_type='float', min_occurs=1, max_occurs=1,
-                mode=MODE.SIMPLE,
-                # allowed_values=(-43.735, -9.005)
+                mode=MODE.SIMPLE, allowed_values=[[-43.735, -9.005]]
             ),
             LiteralInput(
                 'lat_max', 'Latitude maximum value to process in range -43.735:-9.005',
                 data_type='float', min_occurs=1, max_occurs=1,
-                mode=MODE.SIMPLE,
-                # allowed_values=(-43.735, -9.005)
+                mode=MODE.SIMPLE, allowed_values=[[-43.735, -9.005]]
             ),
             LiteralInput(
                 'lon_min', 'Longitude minimum value to process in range 112.905:153.995',
                 data_type='float', min_occurs=1, max_occurs=1,
-                mode=MODE.SIMPLE,
-                # allowed_values=(112.905, 153.995)
+                mode=MODE.SIMPLE, allowed_values=[[112.905, 153.995]]
             ),
             LiteralInput(
                 'lon_max', 'Longitude maximum value to process in range 112.905:153.995',
                 data_type='float', min_occurs=1, max_occurs=1,
-                mode=MODE.SIMPLE,
-                # allowed_values=(112.905, 153.995)
+                mode=MODE.SIMPLE, allowed_values=[[112.905, 153.995]]
             ),
         ]
 
